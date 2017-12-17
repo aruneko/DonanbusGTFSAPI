ac064a134df001dd0a98e0aad79d3f4db4706e44
Aruneko <aruneko99@gmail.com>
2017-12-17 12:13:58 +0900

    バス停の個別検索に対応

--- gtfs_api/apis/stops.py
+++ gtfs_api/apis/stops.py
@@ -5,18 +5,18 @@ from gtfs_api.models import Stop
 stop_namespace = Namespace('stops', description='バス停情報API')
 
 stop = stop_namespace.model('Stop', {
-    'id': fields.String(),
-    'code': fields.String(),
-    'name': fields.String(),
-    'desc': fields.String(),
-    'lat': fields.Float(),
-    'lon': fields.Float(),
-    'zone_id': fields.String(),
-    'url': fields.String(),
-    'location_type': fields.Integer(),
-    'parent_statioin': fields.String(),
-    'timezone': fields.String(),
-    'wheelchair_boarding': fields.Integer(),
+    'id': fields.String(required=True, description='バス停ID', example='0211_B'),
+    'code': fields.String(description='バス停ナンバリング番号', example=''),
+    'name': fields.String(required=True, description='バス停名称', example='東町ターミナル'),
+    'desc': fields.String(description='バス亭付加情報', example='のりば1・2'),
+    'lat': fields.Float(required=True, description='緯度', example='42.3444779'),
+    'lon': fields.Float(required=True, description='経度', example='141.0296731'),
+    'zone_id': fields.String(description='運賃エリアID', example='0211_B'),
+    'url': fields.String(description='バス亭URL', example=''),
+    'location_type': fields.Integer(description='バス停区分', example='0'),
+    'parent_statioin': fields.String(description='親バス停情報', example=',0211'),
+    'timezone': fields.String(description='タイムゾーン', example=None),
+    'wheelchair_boarding': fields.Integer(description='車椅子情報', example=None),
 })
 
 
@@ -25,3 +25,10 @@ class StopsList(Resource):
     @stop_namespace.marshal_list_with(stop)
     def get(self):
         return Stop.query.all()
+
+
+@stop_namespace.route('/<stop_id>')
+class StopController(Resource):
+    @stop_namespace.marshal_with(stop)
+    def get(self, stop_id):
+        return Stop.query.filter(Stop.id == stop_id).first_or_404()
