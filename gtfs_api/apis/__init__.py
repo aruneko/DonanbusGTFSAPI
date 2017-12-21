186db2744eb037da45a49110c08ee68d95053f7d
Aruneko <aruneko99@gmail.com>
2017-12-21 14:51:43 +0900

    複数形に直した

--- gtfs_api/apis/__init__.py
+++ gtfs_api/apis/__init__.py
@@ -1,8 +1,8 @@
 from flask_restplus import Api
 
 from gtfs_api.apis.agency import agency_namespace
-from gtfs_api.apis.fare_attribute import fare_attribute_namespace
-from gtfs_api.apis.fare_rule import fare_rule_namespace
+from gtfs_api.apis.fare_attributes import fare_attribute_namespace
+from gtfs_api.apis.fare_rules import fare_rule_namespace
 from gtfs_api.apis.routes import route_namespace
 from gtfs_api.apis.stops import stop_namespace
 from gtfs_api.apis.trips import trip_namespace
